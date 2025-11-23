eed0a3517de5b84624040af9053213ccce666482
Ian Carlos de Oliveira Leite <ianclrso@gmail.com>
2025-11-22 21:05:17 -0300

    Delete Ecommerce_API directory

--- Ecommerce_API/Domain/Entidades/ItemCarrinho.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Domain.Entidades;
-
-public class ItemCarrinho
-{
-	public int IdCarrinho { get; set; }
-	public int IdProduto { get; set; }
-	public int Quantidade { get; set; }
-	public decimal Preco { get; set; }
-}
--- Ecommerce_API/Domain/Entidades/PedidoItens.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Domain.Entidades;
-public class PedidoItens
-{
-	public int IdPedido { get; set; }
-	public int IdProduto { get; set; }
-	public int Quantidade { get; set; }
-	public decimal Preco { get; set; }
-}
