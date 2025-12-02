afdf713d1ec76f83f633d9997b6d31471940d3c1
Ian Carlos de Oliveira Leite <ianclrso@gmail.com>
2025-12-01 23:40:28 -0300

    Delete Ecommerce_API-main directory

--- Ecommerce_API-main/Domain/Entidades/Pedido.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Domain.Entidades;
-public class Pedido
-{
-    public int IdPedido { get; set; }
-    public int ClienteId { get; set; }
-    public List<PedidoItens> ListaItensPedido { get; set; } = new();
-}
